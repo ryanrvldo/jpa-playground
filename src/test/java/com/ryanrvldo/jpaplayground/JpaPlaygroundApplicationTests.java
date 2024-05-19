5536fc48c2aafeeb1415c6bf74062f1c9ea0919f
Rian Rivaldo <ryanrvldo.linux@gmail.com>
2024-05-19 14:52:11 +0700

    init

---
+++ src/test/java/com/ryanrvldo/jpaplayground/JpaPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryanrvldo.jpaplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
